c998ba451ffd264357c6935e91bf0b8000455dbe
itsshreyashk <shreyashkumar.me@gmail.com>
2024-04-28 15:32:19 +0530

    pikachu

--- client/app/create/new/model/page.tsx
+++ client/app/create/new/model/page.tsx
@@ -1,7 +1,18 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
 const Page: React.FC = () => {
+    const model_name_ref = useRef<HTMLInputElement>(null)
+    const model_description_ref = useRef<HTMLTextAreaElement>(null)
+    const model_icon_url_ref = useRef<HTMLInputElement>(null)
+    const model_source_ref = useRef<HTMLInputElement>(null)
+    const method_selector_ref = useRef<HTMLSelectElement>(null)
+
+
+
+    const archive_btn = useRef<HTMLButtonElement>(null);
+
+    const publish_btn = useRef<HTMLButtonElement>(null);
     useEffect(() => {
         document.title = "Create New Model";
         document.body.style.backgroundColor = "whitesmoke"
@@ -30,18 +41,18 @@ const Page: React.FC = () => {
                                 <h2 className='text-sm font-bold text-gray-600 px-2'>Model Name</h2>
                                 <div className="space-x-0 flex">
                                     <span className='px-4 py-2 border-4 border-r-0 rounded-l-xl text-sm font-bold'>itsshreyashk/</span>
-                                    <input type="text" name="model_name" id="model_name" className='px-4 py-2 w-full rounded-r-xl focus:border-blue-600 font-bold border-4 border-l-0 outline-none text-sm' />
+                                    <input type="text" name="model_name" id="model_name" ref={model_name_ref} className='px-4 py-2 w-full rounded-r-xl focus:border-blue-600 font-bold border-4 border-l-0 outline-none text-sm' />
                                 </div>
                             </div>
                             <div className="description p-3">
                                 <span className='font-bold text-sm w-full text-gray-600 px-2'>Description</span>
-                                <textarea name="model_description" id="model_description" cols={30} rows={10} className='px-4 py-2 w-full border-4 focus:border-blue-600 resize-none rounded-xl outline-none text-sm'>
+                                <textarea name="model_description" id="model_description" ref={model_description_ref} cols={30} rows={10} className='px-4 py-2 w-full border-4 focus:border-blue-600 resize-none rounded-xl outline-none text-sm'>
                                 </textarea>
                             </div>
                             <div className="p-3">
                                 <h2 className='text-sm font-bold text-gray-600 px-2'>Model Icon URL</h2>
                                 <div className="space-x-0 flex">
-                                    <input type="text" name="model_image_url" id="model_image_url" className='px-4 py-2 w-full rounded-xl focus:border-blue-600 font-bold border-4 outline-none text-sm' />
+                                    <input type="text" name="model_image_url" id="model_image_url" ref={model_icon_url_ref} className='px-4 py-2 w-full rounded-xl focus:border-blue-600 font-bold border-4 outline-none text-sm' />
                                 </div>
                             </div>
 
@@ -51,11 +62,11 @@ const Page: React.FC = () => {
                             <h1 className='text-xl font-bold'>Configuration</h1>
                             <div className="p-3">
                                 <h2 className='text-sm font-bold text-gray-600'>Source</h2>
-                                <input type="text" name="model_source" id="model_source" className='px-4 py-2 w-full rounded-xl focus:border-blue-600 font-bold border-4 outline-none text-sm' />
+                                <input type="text" name="model_source" ref={model_source_ref} id="model_source" className='px-4 py-2 w-full rounded-xl focus:border-blue-600 font-bold border-4 outline-none text-sm' />
                             </div>
                             <div className="p-3">
                                 <label className='text-sm font-bold text-gray-600'>Methods</label><br />
-                                <select name="model_method" id="model_method" className='w-full px-4 py-2 rounded-xl text-sm bg-gray-100 font-bold text-gray-800 outline-none border-5 border-gray-700'>
+                                <select name="model_method" id="model_method" ref={method_selector_ref} className='w-full px-4 py-2 rounded-xl text-sm bg-gray-100 font-bold text-gray-800 outline-none border-5 border-gray-700'>
                                     <option value="Default" className=''>Use Default Methods</option>
                                     <option value="" disabled>More coming soon...</option>
 
@@ -64,8 +75,14 @@ const Page: React.FC = () => {
                         </div>
                         <div className="deploy p-2"></div>
                         <div className="publish p-2 w-full flex justify-end">
-                            <button type="button" className='text-sm border px-4 py-2 bg-green-600 text-white active:bg-green-800 outline-none'>Archive</button>
-                            <button type="button" className='text-sm border px-4 py-2 bg-blue-600 text-white active:bg-blue-800 outline-none'>Publish</button>
+                            <button type="button" ref={archive_btn} id='archive_btn' className='text-sm border px-4 py-2 disabled:opacity-70 bg-green-600 text-white active:bg-green-800 outline-none'>Archive</button>
+                            <button type="button" ref={publish_btn} className='text-sm border px-4 py-2 disabled:opacity-70 bg-blue-600 text-white active:bg-blue-800 outline-none' onClick={(e: any) => {
+                                e.target.disabled = true;
+                                if (archive_btn.current) {
+                                    archive_btn.current.disabled = true;
+                                }
+
+                            }}>Publish</button>
                         </div>
                     </div>
                 </div>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,18 +3,22 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import llmface from '../public/llmface-logo.png'
 import Link from "next/link";
+import NextNProgress from 'nextjs-progressbar';
+
 import RestofHomePage from "./HomePage";
 function HomeComponent() {
   useEffect(() => {
-    
+
     document.title = "Home";
-  
+
     return () => {
     }
   }, [])
-  
+
   return (
     <>
+      <NextNProgress />
+
       <nav className="bg-white flex px-2 border-b">
         <div className="w-full flex p-3">
           {/* Use that llmface image as a logo here */}
@@ -27,12 +31,12 @@ function HomeComponent() {
           />
         </div>
         <div className="w-full flex justify-end p-6">
-          <button type="button" className="bg-black text-white px-4 py-2 border active:bg-red-600" onClick={(e : any)=> {
+          <button type="button" className="bg-black text-white px-4 py-2 border active:bg-red-600" onClick={(e: any) => {
             e.target.disabled = true;
             console.log(localStorage.getItem("sid"));
             localStorage.removeItem("sid");
             console.log(localStorage.getItem("sid"));
-            
+
             location.href = "/";
             e.target.disabled = false;
           }}>Logout</button>
@@ -46,6 +50,8 @@ function HomeComponent() {
 function LandingComponent() {
   return (
     <>
+      <NextNProgress />
+
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="px-4 py-2 border">
           <Link href={`/i/flow/login`}>
@@ -65,7 +71,7 @@ export default function Home() {
 
   return (
     <>
-      {( sid !== null && sid !== '' && sid !== undefined) ? (
+      {(sid !== null && sid !== '' && sid !== undefined) ? (
         <HomeComponent />
       ) : (
         <LandingComponent />
