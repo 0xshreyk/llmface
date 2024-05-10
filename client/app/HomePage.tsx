eb1408d883478c4196a19b739454697567377005
itsshreyashk <shreyashkumar.me@gmail.com>
2024-05-10 16:54:01 +0530

    accounts page added

--- client/app/HomePage.tsx
+++ client/app/HomePage.tsx
@@ -34,34 +34,34 @@ const RestofHomePage: React.FC = () => {
                 <div className="w-[100%] max-w-[700px] border-l border-r h-full">
                     <div className="text-center p-2">
                         <Link href={'/create/new/model'}>
-                            <button type="button" className='px-4 py-2 border rounded-xl bg-gray-700 text-white w-3/4 text-sm font-bold active:bg-gray-600'>Create New</button>
+                            <button type="button" className='px-4 py-2 border rounded-xl bg-gray-700 text-white w-3/4 text-sm font-bold active:bg-gray-600 roboto-regular'>Create New</button>
 
                         </Link>
-                        <button type="button" className='px-4 py-2 border rounded-xl bg-green-700 text-white w-3/4 text-sm font-bold active:bg-green-600'>View Analytics</button>
-                        <button type="button" className='px-4 py-2 border rounded-xl bg-blue-700 text-white w-3/4 text-sm font-bold active:bg-blue-600'>View Permissions</button>
+                        <button type="button" className='px-4 py-2 border rounded-xl bg-green-700 text-white w-3/4 text-sm font-bold active:bg-green-600 roboto-regular'>View Analytics</button>
+                        <button type="button" className='px-4 py-2 border rounded-xl bg-blue-700 text-white w-3/4 text-sm font-bold active:bg-blue-600 roboto-regular'>View Permissions</button>
                     </div>
                     <div className="px-4 py-2">
-                        <h1 className='font-bold text-blue-600'>Your Models </h1>
+                        <h1 className='font-bold text-blue-600 roboto-bold'>Your Models </h1>
                     </div>
                     <div className="models p-2">
                         <table border={1} className='w-full border-2 border-gray-200 px-4 py-2 rounded-xl'>
                             <tr className='overflow-hidden'>
-                                <th className='text-sm text-gray-700 px-4 py-2'>Model Name</th>
-                                <th className='text-sm text-gray-700 px-4 py-2'>Created</th>
-                                <th className='text-sm text-gray-700 px-4 py-2'>Owner</th>
-                                <th className='text-sm text-gray-700 px-4 py-2'>Options</th>
+                                <th className='text-sm text-gray-700 px-4 py-2 roboto-regular'>Model Name</th>
+                                <th className='text-sm text-gray-700 px-4 py-2 roboto-regular'>Created</th>
+                                <th className='text-sm text-gray-700 px-4 py-2 roboto-regular'>Owner</th>
+                                <th className='text-sm text-gray-700 px-4 py-2 roboto-regular'>Options</th>
                             </tr>
                             {
                                 cacheModels.map((element, index) => {
                                     return (<tr className='p-2 hover:bg-gray-300 cursor-pointer overflow-hidden' key={element.model_id}>
                                         <td className='px-4 py-2 text-sm text-blue-600 font-bold cursor-pointer hover:underline'>{element.model_name}</td>
                                         <td className='px-4 py-2 text-sm text-gray-700'>{element.createdAt}</td>
-                                        <td className='px-4 py-2 text-sm text-blue-600 hover:underline font-bold'>{element.model_owner}</td>
+                                        <td className='px-4 py-2 text-sm text-blue-600 hover:underline font-bold roboto-bold'>{element.model_owner}</td>
                                         <td className='px-4 py-2 text-sm flex space-x-2'>
-                                            <button type="button" className='text-sm border border-gray-100 px-4 py-2 rounded-lg hover:bg-red-600 duration-200 hover:text-white flex'><span className="material-symbols-outlined">
+                                            <button type="button" className='text-sm border roboto-regular border-gray-100 px-4 py-2 rounded-lg hover:bg-red-600 duration-200 hover:text-white flex'><span className="material-symbols-outlined">
                                                 delete
                                             </span></button>
-                                            <button type="button" className='text-sm border border-gray-100 px-4 py-2 rounded-lg hover:bg-green-600 duration-200 hover:text-white flex'><span className="material-symbols-outlined">
+                                            <button type="button" className='text-sm border roboto-regular border-gray-100 px-4 py-2 rounded-lg hover:bg-green-600 duration-200 hover:text-white flex'><span className="material-symbols-outlined">
                                                 edit
                                             </span></button>
                                         </td>
