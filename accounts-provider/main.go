97275de633b6aec6069738801c0e6332ab01062f
Shreyash Kumar <147302693+itsshreyashk@users.noreply.github.com>
2024-05-24 18:39:44 +0530

    correct

--- accounts-provider/main.go
+++ accounts-provider/main.go
@@ -12,10 +12,18 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"encoding/json"
+
 	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func delay() any {
+	time.Sleep(2 * time.Second)
+	return 3
+}
 func main() {
 	ctx := context.Background()
 
@@ -38,20 +46,41 @@ func main() {
 			panic(err)
 		}
 	}()
-	coll := client.Database("llmface").Collection("users")
-	title := "Let's see how it goes"
+	coll := client.Database("llmface").Collection("users") // selecting
 
 	r := mux.NewRouter()
 	r.HandleFunc("/accounts/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Health is up")
+		w.Write([]byte("Healthy"))
 	})
-	r.HandleFunc("/get/account/info/{id}", func(w http.ResponseWriter, r *http.Request) {
+	/*
+	* Function taking the username (id)
+	 */
+	 r.HandleFunc("/get/account/info/{id}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id := vars["id"]
 		fmt.Print(id)
+	
+		filter := bson.D{{"creds.username", id}}
+	
+		var result bson.M
+		err := coll.FindOne(context.TODO(), filter).Decode(&result)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				http.Error(w, "User not found", http.StatusNotFound)
+				return
+			}
+			log.Fatal(err)
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(""))
+		response, err := json.Marshal(result)
+		if err != nil {
+			http.Error(w, "Error marshalling response", http.StatusInternalServerError)
+			return
+		}
+		w.Write(response)
 	})
-
+	
 	http.ListenAndServe(":8080", r)
 }
